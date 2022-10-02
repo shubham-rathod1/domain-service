6e4e1ae980431a8b6ccecbd0ed015362a94ea71f
shubham <shubs12@gmail.com>
2022-10-02 12:52:04 +0530

    hardhat setup

---
+++ contracts/Domains.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("basic contract created");
+    }
+}
